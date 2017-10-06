66bf92494f05573755bb187d6ae0562fe29637f6
Loloshik <32200252+Loloshik@users.noreply.github.com>
2017-10-06 20:27:33 +0300

    Add
    Count etc.

--- CPO/CPO/Stack.cs
+++ CPO/CPO/Stack.cs
@@ -35,6 +35,57 @@ namespace CPO
                     set { this._Prev = value; }
                 }
             }
+            private Node First;
+            private int size;
+
+            public MyStack()
+            {
+                size = 0;
+                First = null;
+            }
+
+
+            public int Count //параметр для вывода размера 
+            {
+                get { return size; }
+                set { size = value; }
+            }
+
+            public T Pop() //метод взятия с вершины
+            {
+                if (First == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                else
+                {
+                    Node temp = First;
+                    if (First.Next != null)
+                    {
+                        First.Next.Prev = null;
+                    }
+                    First = First.Next;
+                    Count--;
+                    return temp.Value;
+                }
+            }
+
+            public void Push(T newElement)
+            {
+                Node newNode = new Node(newElement);
+
+                if (First == null)
+                {
+                    First = newNode;
+                }
+                else
+                {
+                    newNode.Next = First;
+                    First = newNode; //First и newNode указывают на один и тот же объект
+                    newNode.Next.Prev = First;
+                }
+                Count++;
+            }
         }
     }
 }
