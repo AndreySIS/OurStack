bb7e003982bc6af0754cba237e60448c30e3f7b0
Loloshik <32200252+Loloshik@users.noreply.github.com>
2017-10-06 20:47:13 +0300

    Interface
    +

---
+++ CPO/CPO/IStack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPO
+{
+    interface IStack<T>
+    {
+        int Count { get; set; }
+        T Pop();
+        void Push(T newElement);
+
+    }
+
+}
